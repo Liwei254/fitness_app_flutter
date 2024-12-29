60d08ebae81649519d85919cd291e9c5e3e8133f
Otipa <otipajames2@gmail.com>
2024-12-29 15:13:46 +0300

    restructured my project

---
+++ lib/pages/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
